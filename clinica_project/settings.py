7603dc4ae69e6f621611437a31bd192e6cfcffd0
ValLR <valentinalopezrecabal@gmail.com>
2025-09-18 16:37:51 -0300

    feat: Agregar databases a settings.py

--- clinica_project/settings.py
+++ clinica_project/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'clinica_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'localhost:1521/XEPDB1',
+        'USER': 'clinica',
+        'PASSWORD': 'ClinicaBV_Orcl21c!Cedro79',
     }
 }
 
