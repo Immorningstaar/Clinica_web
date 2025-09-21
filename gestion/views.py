f621af1f0441e4055ae03a7be20ce89f619aa28b
jeremiAbgrund <jeremiriquelme@gmail.com>
2025-09-21 13:23:52 -0300

    SEG-01: Importa settings en views para usar DEBUG y corregir 'settings' no definido

--- gestion/views.py
+++ gestion/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from django.contrib.auth.models import User
